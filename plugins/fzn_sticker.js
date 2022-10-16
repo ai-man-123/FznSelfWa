50674405a499140732a3d0363678bf957e6a9cde
AIMAN *͘⁴̅⁰͍⁴̵〆* <82744566+ai-man-123@users.noreply.github.com>
2022-10-16 09:52:32 +0800

    Update fzn_sticker.js

--- plugins/fzn_sticker.js
+++ plugins/fzn_sticker.js
@@ -5,12 +5,12 @@
 					const getbuff = isQuotedImage ? JSON.parse(JSON.stringify(msg).replace('quotedM','m')).message.extendedTextMessage.contextInfo : msg
 					const dlfile = await fzn.downloadMediaMessage(getbuff)
 					const bas64 = `data:image/jpeg;base64,${dlfile.toString('base64')}`
-					Kirim.GambarJadiStiker(from, bas64, msg, {author: 'Fazone', pack: 'SELF'})
+					Kirim.GambarJadiStiker(from, bas64, msg, {author: 'Aiman', pack: 'SELF'})
 					} else if ((isMedia && msg.message.videoMessage.seconds < 11 || isQuotedVideo && msg.message.extendedTextMessage.contextInfo.quotedMessage.videoMessage.seconds < 11)){
 					const getbuff = isQuotedVideo ? JSON.parse(JSON.stringify(msg).replace('quotedM','m')).message.extendedTextMessage.contextInfo : msg
 					const dlfile = await fzn.downloadMediaMessage(getbuff)
 					const bas64 = `data:video/mp4;base64,${dlfile.toString('base64')}`
-					Kirim.VideoJadiStiker(from, bas64, msg, {author: 'Fazone', pack: 'SELF'})	 
+					Kirim.VideoJadiStiker(from, bas64, msg, {author: 'Aiman', pack: 'SELF'})	 
 					} else {
 					reply(`Kirim gambar/video dengan caption ${prefix}sticker atau tag gambar/video yang sudah dikirim\nNote : Durasi video maximal 10 detik`)
 					}
